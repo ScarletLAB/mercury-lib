47c9511f3af3278424f03cca5a063928d6164f5f
ScarletLAB <ecsclanmail@gmail.com>
2025-02-09 19:42:11 +0200

    Update src.lua

--- src.lua
+++ src.lua
@@ -40,7 +40,7 @@ local Library = {
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
 		Serika = {
-			Main = Color3.fromRGB(352, 69, 84),
+			Main = Color3.fromRGB(18, 18, 19),
 			Secondary = Color3.fromRGB(80, 82, 85),
 			Tertiary = Color3.fromRGB(226, 183, 20),
 
